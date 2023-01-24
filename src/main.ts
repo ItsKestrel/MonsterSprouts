015fcaa157eaf64038e2662378e5f40493d6e5de
Rigidity <me@rigidnetwork.com>
2023-01-24 16:50:24 -0500

    Fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, '..', 'dist', 'preload.js'),
+        },
     });
 
-    win.loadFile(__dirname + '/index.html');
-};
+    mainWindow.loadFile(path.join(__dirname, '..', 'dist', 'index.html'));
+}
 
 app.whenReady().then(() => {
     createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
