3e881046a893e12afdb1ea6a65954b7acde6ff9e
ItsKestrel <welford.ryan@gmail.com>
2023-01-26 13:18:59 -0600

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import './index.css';
+
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Market from './pages/Market';
-import Main from './pages/Main';
+import Home from './pages/Home';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Main />} />
+                <Route path='/' element={<Home loggedIn={true} />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/game' element={<Game />} />
                 <Route path='/market' element={<Market />} />
---
+++ src/components/Stash.tsx
@@ -0,0 +1,40 @@
+import Card from './Card';
+
+export default function Stash() {
+    const cardData = {
+        title: 'Sword',
+        image: 'https://cdn-icons-png.flaticon.com/512/842/842184.png',
+        type: 'weapon',
+        
+    };
+    const deck = [
+        {
+            cardData: cardData
+        },
+        {
+            cardData: cardData
+        },
+        {
+            cardData: cardData
+        },
+        {
+            cardData: cardData
+        },
+        {
+            cardData: cardData
+        },
+    ];
+
+
+
+
+    return (
+        <div className="grid grid-cols-4 gap-4">
+            {deck.map((card) => {
+                return (
+                    <Card cardData={card.cardData} />
+                )
+            })}
+        </div>
+    )
+}
