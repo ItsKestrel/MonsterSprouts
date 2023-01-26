75a0686404af38ea04f6bc2238e8b5efee65c16d
Rigidity <me@rigidnetwork.com>
2023-01-26 15:28:19 -0500

    Monorepo

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,22 @@
+import './index.css';
+
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Game from './pages/Game';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Market from './pages/Market';
+
+function App() {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Home loggedIn={true} />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/game" element={<Game />} />
+                <Route path="/market" element={<Market />} />
+            </Routes>
+        </HashRouter>
+    );
+}
+
+export default App;
