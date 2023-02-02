15878d3e7ec8bd4201112d0062dc5fc69fe7d0ae
Rigidity <me@rigidnetwork.com>
2023-02-02 15:03:08 -0500

    Change port name

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -13,6 +13,6 @@ app.get('/api/v1/test', (_req, res) => {
     res.send(response);
 });
 
-const port = process.env.PORT!;
+const port = process.env.SERVER_PORT!;
 
 app.listen(port, () => console.log(`Server started on port ${port}.`));
