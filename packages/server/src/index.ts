6a624befb50eda61b346677450a471b31b4bad96
mando222 <evemsmando@gmail.com>
2023-02-07 16:19:48 -0500

    test

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,26 @@
 import express from 'express';
+import { Test, NewGame } from '../../shared/src';
+import {v4 as uuidv4} from 'uuid';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/api/v1/test', (_req, res) => {
+    let response: Test = {
+        something: 'hello',
+    };
+
+    res.send(response);
+});
+
+app.get('/api/v1/new-game', (_req, res) => {
+    let response: NewGame = {
+        gameid: uuidv4()
+    }
+});
+
+//test
 
 const port = process.env.SERVER_PORT!;
 
