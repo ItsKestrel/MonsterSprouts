ee9edc164ce4621b4f41a20a643de355ced6e4ee
ItsKestrel <welford.ryan@gmail.com>
2023-02-09 13:57:06 -0600

    Update Login.tsx

--- packages/client/src/pages/Login.tsx
+++ packages/client/src/pages/Login.tsx
@@ -1,7 +1,14 @@
+import Button from '@mui/material/Button';
+
 export default function Login() {
+    const handleConnect = () => {
+        console.log('CONNECT!');
+    }
     return (
         <div>
             <h1>Login</h1>
+            <Button onClick={handleConnect}>Connect with WalletConnect</Button>
+
         </div>
     );
 }
