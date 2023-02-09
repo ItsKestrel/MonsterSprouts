b2df94742dfa0dbaa5e07c3974659de9e5a982bd
Rigidity <me@rigidnetwork.com>
2023-02-09 14:55:53 -0500

    Add other pages back

---
+++ packages/client/src/pages/Market.tsx
@@ -0,0 +1,13 @@
+export default function Market() {
+    return (
+        <div>
+            <h1>Market</h1>
+            <div className="grid grid-cols-2">
+                <h3>This</h3>
+                <h3>Is</h3>
+                <h3>A</h3>
+                <h3>Grid</h3>
+            </div>
+        </div>
+    );
+}
