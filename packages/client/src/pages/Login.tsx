a465ce9e3a24526c43d02adb27d7ceba85e54227
Rigidity <me@rigidnetwork.com>
2023-02-09 15:10:33 -0500

    Add dev deps

--- packages/client/src/pages/Login.tsx
+++ packages/client/src/pages/Login.tsx
@@ -1,14 +1,15 @@
 import Button from '@mui/material/Button';
+import SignClient from '@walletconnect/sign-client';
 
 export default function Login() {
     const handleConnect = () => {
         console.log('CONNECT!');
-    }
+        SignClient;
+    };
     return (
         <div>
             <h1>Login</h1>
             <Button onClick={handleConnect}>Connect with WalletConnect</Button>
-
         </div>
     );
 }
